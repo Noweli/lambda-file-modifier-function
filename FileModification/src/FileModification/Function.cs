7dfac3f647744bf359c19dade2eb9a927f459b40
Patryk Lewon <patryklewonn@gmail.com>
2023-02-04 15:34:07 +0100

    Entry s3 handling

--- FileModification/src/FileModification/Function.cs
+++ FileModification/src/FileModification/Function.cs
@@ -1,6 +1,10 @@
 using System.Threading.Tasks;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
+using Amazon.S3;
+using Amazon.S3.Model;
+using FileModification.Models;
+using FileModification.Resources;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -9,9 +13,31 @@ namespace FileModification
 
     public class Function
     {
-
-        public async Task FunctionHandler(S3Event s3Event, ILambdaContext context)
+        public async Task<Result> FunctionHandler(S3Event s3Event, ILambdaContext context)
         {
+            var objectKey = s3Event.Records[0].S3.Object.Key;
+            var bucketName = s3Event.Records[0].S3.Bucket.Name;
+
+            if (string.IsNullOrWhiteSpace(objectKey))
+            {
+                return new Result(false, ErrorMessages.S3Validation_KeyIsNullOrEmpty);
+            }
+            
+            if (string.IsNullOrWhiteSpace(bucketName))
+            {
+                return new Result(false, ErrorMessages.S3Validation_BucketNameIsNullOrEmpty);
+            }
+
+            var s3Client = new AmazonS3Client();
+
+            var objectAttributesRequest = new GetObjectAttributesRequest
+            {
+                BucketName = bucketName,
+                Key = objectKey
+            };
+
+            //Get object metadata
+            var objectAttributes = await s3Client.GetObjectAttributesAsync(objectAttributesRequest);
             
         }
     }
---
+++ FileModification/src/FileModification/Models/Result.cs
@@ -0,0 +1,13 @@
+﻿namespace FileModification.Models;
+
+public class Result
+{
+    private bool IsSuccessful { get; }
+    private string ErrorMessage { get; }
+
+    public Result(bool isSuccessful, string errorMessage = "")
+    {
+        IsSuccessful = isSuccessful;
+        ErrorMessage = errorMessage;
+    }
+}
